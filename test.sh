590bc32533ab328b1e38814c0d81d877d379c20b
vsoch <vsochat@stanford.edu>
2017-09-09 15:57:19 -0700

    fixing csh script

--- test.sh
+++ test.sh
@@ -23,25 +23,29 @@ for app in $(singularity apps $container)
     echo $app
 done
 
-echo "Display running commands:"
+echo "\nDisplay running commands:"
+echo "###################################"
 for app in $(singularity apps $container)
     do
     echo "singularity run --app $app $container"
 done
 
-echo "Running for each language..."
+echo "\nRunning for each language..."
+echo "###################################"
 for app in $(singularity apps $container)
     do
     singularity run --app $app $container
 done
 
 
-echo "Results will be written to ${TIME_LOG}"
+echo "\nResults will be written to ${TIME_LOG}"
 
 export TIME='%C\t%E\t%K\t%I\t%M\t%O\t%P\t%U\t%W\t%X\t%e\t%k\t%p\t%r\t%s\t%t\t%w\n'
 echo -e 'COMMAND\tELAPSED_TIME_HMS\tAVERAGE_MEM\tFS_INPUTS\tMAX_RES_SIZE_KB\tFS_OUTPUTS\tPERC_CPU_ALLOCATED\tCPU_SECONDS_USED\tW_TIMES_SWAPPED\tSHARED_TEXT_KB\tELAPSED_TIME_SECONDS\tNUMBER_SIGNALS_DELIVERED\tAVG_UNSHARED_STACK_SIZE\tSOCKET_MSG_RECEIVED\tSOCKET_MSG_SENT\tAVG_RESIDENT_SET_SIZE\tCONTEXT_SWITCHES' > $TIME_LOG
 
-echo "Performing Tests"
+echo "\nPerforming Tests"
+echo "###################################"
+
 for app in $(singularity apps $container)
     do
     /usr/bin/time -a -o $TIME_LOG singularity run --app $app $container >> $TIME_LOG
